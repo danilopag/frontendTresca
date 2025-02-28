45c97ea0a90906da0d428dc674718fb33ff24f60
danilopag <danilopaglialunga99@gmail.com>
2025-02-28 01:25:01 +0100

    Aggiornato api.js

--- utils/api.js
+++ utils/api.js
@@ -5,7 +5,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api'
+    baseURL: 'https://backendtresca-production.up.railway.app/api'
 });
 
 api.interceptors.request.use(
