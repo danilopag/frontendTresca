3cb72ca9c8664b94c5130ffd7e94de4b4345663c
danilopag <danilopaglialunga99@gmail.com>
2025-03-06 16:48:26 +0100

    Fix api.js

--- utils/api.js
+++ utils/api.js
@@ -5,8 +5,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    //baseURL: 'https://backendtresca-production.up.railway.app/api'
-    baseURL: 'http://localhost:5000/api'
+    baseURL: 'https://backendtresca-production.up.railway.app/api'
+    //baseURL: 'http://localhost:5000/api'
 });
 
 api.interceptors.request.use(
