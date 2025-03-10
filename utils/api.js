1c1a7d69d9462e03d09f8fbbd374362f98b9a58c
danilopag <danilopaglialunga99@gmail.com>
2025-03-10 16:15:09 +0100

    Test Vercel

--- utils/api.js
+++ utils/api.js
@@ -6,7 +6,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api'
+    //baseURL: 'http://localhost:5000/api'
+    baseURL: 'https://backendtresca-production.up.railway.app/api'
 });
 
 api.interceptors.request.use(
